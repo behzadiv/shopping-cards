b010ad7021c1c138a731a76a619d85becc40d8dd
Behzad <saeedibehzad@gmail.com>
2021-11-21 14:19:37 +0330

    get static data and map on  it and show in HomePage + styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,25 @@
 import Layout from "../Layout/layout";
+import "./HomePage.css"
 
 const HomePage = () => {
+    const addProductHandler =(product)=>{
+        console.log(product)
+    }
     return ( 
-        
-            <h2>this is home page</h2>
+        <main className="productList container">
+            {data.products.map((product)=>
+            <section className="product">
+                <div className="productImg">
+                    <img src={product.image} alt={product.name}/>
+                </div>
+                <div className="productDesc">
+                    <h4>{product.name}</h4>
+                    <p> $ {product.price}</p>
+                    <button onClick={()=>addProductHandler(product)} className="btn primary">Add</button>
+                </div>
+            </section>    
+            )}
+        </main>
         
      );
 }
