7eefb160a1cd1846d8a05a0d07fd0fe4d57e8a98
Behzad <saeedibehzad@gmail.com>
2021-11-25 09:54:31 +0330

    update card & total in cartReducer

--- src/provider/cartReducer.js
+++ src/provider/cartReducer.js
@@ -1,28 +1,43 @@
-
 const cartReducer = (state, action) => {
-                switch (action.type) {
-                case "ADD_TO_CART":
-                    console.log(action,state);
-                    const updatedCart = [...state.cart]
-                    const updatedItemIndex = updatedCart.findIndex((item)=>item.id ===action.payload.id)
-                    if(updatedItemIndex < 0){
-                       updatedCart.push({...action.payload,quantity:1})
-                       console.log(updatedCart);
-                    }
-                    else{
-                        const selectedCart={...updatedCart[updatedItemIndex]}
-                        selectedCart.quantity ++
-                        updatedCart[updatedItemIndex]=selectedCart
-                    }
-                    return{...state , cart : updatedCart}
-                    break;
-                    
-                    default:
-                        break;
-                    }
-                    
-                }
+  switch (action.type) {
+    case "ADD_TO_CART":
+      console.log(action, state);
+      //console.log(state.total);
+      const updatedCart = [...state.cart];
+      const updatedItemIndex = updatedCart.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (updatedItemIndex < 0) {
+        updatedCart.push({ ...action.payload, quantity: 1 });
+        } else {
+        const selectedCart = { ...updatedCart[updatedItemIndex] };
+        selectedCart.quantity++;
+        updatedCart[updatedItemIndex] = selectedCart;
+      }
+      return { ...state, cart: updatedCart , total:state.total + action.payload.price };
+      break;
+    
+    case "DECREMENT_CART_QTY": {
+      const updatedCart = [...state.cart];
+      let totalPrice = state.total
+      const updatedItemIndex = updatedCart.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if(action.payload.quantity>1){
+        const selectedCart = { ...updatedCart[updatedItemIndex] };
+        selectedCart.quantity--;
+        updatedCart[updatedItemIndex] = selectedCart;
+        return { ...state, cart: updatedCart,total:state.total -action.payload.price }
+      }else{
+        const filteredCarts = updatedCart.filter((cart)=>cart.id !==action.payload.id)
+        console.log(filteredCarts);
+        return { ...state, cart: filteredCarts,total:state.total -action.payload.price  }
+    }
+      ;
+    }
+    default:
+      break;
+  }
+};
 
-
- 
 export default cartReducer;
