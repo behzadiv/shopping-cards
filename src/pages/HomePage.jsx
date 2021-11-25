80ae30db6c00ea8b7cd5d9012db9a42f4de98515
Behzad <saeedibehzad@gmail.com>
2021-11-25 12:16:04 +0330

    create a utils folder and CheckInCart function in it

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import Layout from "../Layout/layout";
 import * as data from "../data"
 import "./HomePage.css"
-import { useCartActions } from "../provider/CartProvider";
+import { useCart, useCartActions } from "../provider/CartProvider";
+import { NavLink } from "react-router-dom"
+import { checkInCart } from "../utils/CheckInCart";
 
 const HomePage = () => {
+    const {cart}=useCart()
     const dispatch = useCartActions()
+    const addHandler=(product)=>{
+        dispatch({type:"ADD_TO_CART",payload:product})
+    }
     return ( 
         <main className="productList container">
             {data.products.map((product)=>
@@ -15,7 +21,7 @@ const HomePage = () => {
                 <div className="productDesc">
                     <h4>{product.name}</h4>
                     <p> $ {product.price}</p>
-                    <button onClick={()=>dispatch({type:"ADD_TO_CART",payload:product})} className="btn primary">Add</button>
+                    <button onClick={()=>addHandler(product)} className="btn primary">{checkInCart(cart ,product)? "Cart in":"Add to cart"}</button>
                 </div>
             </section>    
             )}
