088520b05a923d5824d77466c2198ef19bc33cc0
Behzad <saeedibehzad@gmail.com>
2021-11-28 15:07:26 +0330

    use authProvider in App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import routes from "./routes";
 import Layout from "./Layout/layout";
 import CartProvider from "./provider/CartProvider";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import AuthProvider from "./provider/AuthProvider";
 function App() {
   return (
-    <CartProvider>
-      <Layout>
-        <ToastContainer />
-        <Routes>
-          {routes.map((route) => (
-            <Route {...route} key={route.path}/>
-          ))}
-          {/* <Route path="/" element={<HomePage />} />
+    <AuthProvider>
+      <CartProvider>
+        <Layout>
+          <ToastContainer />
+          <Routes>
+            {routes.map((route) => (
+              <Route {...route} key={route.path} />
+            ))}
+            {/* <Route path="/" element={<HomePage />} />
         <Route path="/cards" element={<Cards />} /> */}
-        </Routes>
-      </Layout>
-    </CartProvider>
+          </Routes>
+        </Layout>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
