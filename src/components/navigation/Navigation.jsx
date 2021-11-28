1750b77a50b48b27025a9c443db8e9193840d967
Behzad <saeedibehzad@gmail.com>
2021-11-28 15:08:20 +0330

    change navigation and when user logged in show profile item

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,19 +1,41 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../../provider/AuthProvider";
 import { useCart } from "../../provider/CartProvider";
-import "./Navigation.css"
+import "./Navigation.css";
 const Navigation = () => {
-    const {cart} = useCart()
-    console.log(cart);
-    return ( 
-        <header className="mainNavigation">
-            <nav>
-            <ul>
-                <li><NavLink to="/" className={(navData)=> navData.isActive? "activeLink" : ""} exact>Home</NavLink></li>
-                <li><NavLink to="/cards" className={(navData)=> navData.isActive? "activeLink" : ""}>Cards<span className="basket">{cart.length}</span></NavLink></li>
-            </ul>
-        </nav>
-        </header>
-     );
-}
- 
+  const { cart } = useCart();
+  const userData= useAuth()
+  console.log(userData);
+  return (
+    <header >
+      <nav className="mainNavigation">
+        <ul>
+          <h5>Behzad Shopping</h5>
+          <li>
+            <NavLink
+              to="/"
+              className={(navData) => (navData.isActive ? "activeLink" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <NavLink
+              to="/carts"
+              className={(navData) => (navData.isActive ? "activeLink" : "")}
+            >
+              Cards<span className="basket">{cart.length}</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={!userData? "/login" :"/profile"}>{!userData? "login / signup" : "profile"}</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
 export default Navigation;
