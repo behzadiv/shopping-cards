57cac5cf5617e43a14935770bb0edbbe83d36db8
Behzad <saeedibehzad@gmail.com>
2021-11-28 18:36:35 +0330

    persist user in app

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,10 +1,15 @@
-import {React,useState,useContext, createContext} from "react";
+import {React,useState,useContext, createContext, useEffect} from "react";
 
 const AuthContext = createContext()
 const AuthContextDispatcher = createContext()
 
 const AuthProvider = ({children}) => {
     const[state,setState]=useState(false)
+    useEffect(()=>{
+        const userData = JSON.parse(localStorage.getItem("authState")) || false
+        setState(userData)
+    },[])
+    
     return ( 
         <AuthContext.Provider value={state} >
             <AuthContextDispatcher.Provider value={setState}>
